c49c18735d6ba34c0c7a45df9d920b452110fef6
KEM-CONSOLATION <consolationlotachi@gmail.com>
2024-06-08 15:28:38 +0100

    Adding Team Images

--- src/Component/Team.jsx
+++ src/Component/Team.jsx
@@ -1,16 +1,18 @@
-import Team1 from "../assets/Team (2).svg";
-import Team2 from "../assets/Team (3).svg";
-import Team3 from "../assets/Team (4).svg";
+import { useEffect } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Aos from "aos";
 import "aos/dist/aos.css";
-import { useEffect } from "react";
+import Team1 from "../assets/Akindele Marvellous Olakiiitan.jpg";
+import Team2 from "../assets/ED.jpeg";
+import Team3 from "../assets/Team (4).svg";
+
 const Team = () => {
   useEffect(() => {
     Aos.init({ duration: 2000 });
   }, []);
+
   const settings = {
     dots: false,
     infinite: true,
@@ -49,76 +51,64 @@ const Team = () => {
     ],
   };
 
+  const teamData = [
+    {
+      name: "Akindele Marvellous Olakiiitan",
+      position: "Operations Lead, Script Desk",
+      image: Team1,
+      aos: "fade-left",
+    },
+    {
+      name: "Ramsey Bucknor",
+      position: "HR Manager",
+      image: Team2,
+      aos: "fade-right",
+    },
+    {
+      name: "Ramsey Bucknor",
+      position: "HR Manager",
+      image: Team3,
+      aos: "fade-down",
+    },
+  ];
+
   return (
     <div
-      className="overflow-hidden  max-w-5xl  md:mx-auto mx-5 2xl:max-w-full 2xl:mx-40 py-[40px]"
+      className="overflow-hidden max-w-5xl md:mx-auto mx-5 2xl:max-w-full 2xl:mx-40 py-[40px]"
       id="team"
     >
-      <p className=" text-[#333333] text-[14px] lg:text-[15px] mb-[22px]">
+      <p className="text-[#333333] text-[14px] lg:text-[15px] mb-[22px]">
         INSIDE SCRIPTDESK
       </p>
-      <p className=" font-[500] text-[18px] lg:text-[20px] text-[#333333] ">
+      <p className="font-[500] text-[18px] lg:text-[20px] text-[#333333]">
         MEET THE TEAM
       </p>
 
-      <div className=" mt-[44px]">
+      <div className="mt-[44px]">
         <Slider {...settings} className="">
-          <div
-            className=" max-w-[300px] 2xl:w-[600px] max-h-[510px] rounded-[10px]"
-            data-aos="fade-left"
-          >
-            <img
-              src={Team1}
-              alt=""
-              className=" bg-cover transition-transform duration-1000 transform scale-100 hover:scale-110"
-            />
-            <div className=" py-[24px]">
-              <p className=" text-[#333333] text-[20px] font-[500]">
-                Ramsey Bucknor
-              </p>
-              <p className=" text-[#616161] text-[18px] font-[400] mt-[15px]">
-                HR Manager
-              </p>
+          {teamData.map((member, index) => (
+            <div
+              key={index}
+              className="max-w-[300px] 2xl:w-[600px] max-h-[510px] rounded-[10px]"
+              data-aos={member.aos}
+            >
+              <div className="h-[348px] overflow-hidden rounded-[10px]">
+                <img
+                  src={member.image}
+                  alt={member.name}
+                  className="h-full w-full object-cover transition-transform duration-1000 transform scale-100 hover:scale-110"
+                />
+              </div>
+              <div className="py-[24px]">
+                <p className="text-[#333333] text-[20px] font-[500]">
+                  {member.name}
+                </p>
+                <p className="text-[#616161] text-[18px] font-[400] mt-[15px]">
+                  {member.position}
+                </p>
+              </div>
             </div>
-          </div>
-
-          <div
-            className="max-w-[300px] 2xl:w-[600px] max-h-[510px] rounded-[10px]"
-            data-aos="fade-right"
-          >
-            <img
-              src={Team2}
-              alt=""
-              className=" bg-cover transition-transform duration-1000 transform scale-100 hover:scale-110"
-            />
-            <div className=" py-[24px]">
-              <p className=" text-[#333333] text-[20px] font-[500]">
-                Ramsey Bucknor
-              </p>
-              <p className=" text-[#616161] text-[18px] font-[400] mt-[15px]">
-                HR Manager
-              </p>
-            </div>
-          </div>
-
-          <div
-            className=" max-w-[300px] 2xl:w-[600px] max-h-[510px] rounded-[10px]"
-            data-aos="fade-down"
-          >
-            <img
-              src={Team3}
-              alt=""
-              className=" bg-cover transition-transform duration-1000 transform scale-100 hover:scale-110"
-            />
-            <div className=" py-[24px]">
-              <p className=" text-[#333333] text-[20px] font-[500]">
-                Ramsey Bucknor
-              </p>
-              <p className=" text-[#616161] text-[18px] font-[400] mt-[15px]">
-                HR Manager
-              </p>
-            </div>
-          </div>
+          ))}
         </Slider>
       </div>
     </div>
